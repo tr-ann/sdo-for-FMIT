707a8eb6885d4f567b70a08003c5e7560f1ccba2
Ann Trepachko <trepachko.ann@gmail.com>
2019-11-16 16:42:52 +0300

    download sequelize&mysql2 + db structure

---
+++ sql/links.sql
@@ -0,0 +1,98 @@
+/* таблица аудитории */
+ALTER TABLE lecture_room
+    ADD CONSTRAINT FK_lecture_room_to_room_type     FOREIGN KEY(type_id)    REFERENCES room_type(id),
+		ADD CONSTRAINT FK_lecture_room_to_building     FOREIGN KEY(building_id)    REFERENCES building(id);
+			
+/* таблица кафедра */
+ALTER TABLE department
+    ADD CONSTRAINT FK_department_to_user FOREIGN KEY(owner_id) REFERENCES user(id),
+        ADD CONSTRAINT FK_department_to_lecture_room FOREIGN KEY(room_id) REFERENCES lecture_room(id);
+	
+/* таблица пользователь */
+ALTER TABLE user_role
+	ADD CONSTRAINT FK_user_role_to_role FOREIGN KEY(role_id) REFERENCES role(id),
+		ADD CONSTRAINT FK_user_role_to_user FOREIGN KEY(user_id) REFERENCES user(id);
+    
+/* таблица телефон */
+ALTER TABLE phone
+	ADD CONSTRAINT FK_phone_to_user FOREIGN KEY(user_id) REFERENCES user(id);
+	
+/* таблица информация о пользователе */
+ALTER TABLE user_info
+    ADD CONSTRAINT FK_user_info_to_user FOREIGN KEY(user_id) REFERENCES user(id),
+        ADD CONSTRAINT FK_user_info_to_source FOREIGN KEY(photo_id) REFERENCES source(id);
+
+/* таблица преподаватель */
+ALTER TABLE teacher
+    ADD CONSTRAINT FK_teacher_to_user FOREIGN KEY(id) REFERENCES user(id),
+        ADD CONSTRAINT FK_teacher_to_department FOREIGN KEY(department_id) REFERENCES department(id),
+            ADD CONSTRAINT FK_teacher_to_position FOREIGN KEY(position_id) REFERENCES position(id),
+                ADD CONSTRAINT FK_teacher_to_academic_degree FOREIGN KEY(academic_degree_id) REFERENCES academic_degree(id),
+                    ADD CONSTRAINT FK_teacher_to_academic_rank FOREIGN KEY(academic_rank_id) REFERENCES academic_rank(id);
+							
+/* таблица информация о факультете */
+ALTER TABLE info_faculty
+	ADD CONSTRAINT FK_info_faculty_to_faculty FOREIGN KEY(id) REFERENCES faculty(id);
+						
+/* таблица группа */
+ALTER TABLE group
+	ADD CONSTRAINT FK_group_to_faculty FOREIGN KEY(faculty_id) REFERENCES faculty(id),
+	    ADD CONSTRAINT FK_group_to_specialty FOREIGN KEY(specialty_id) REFERENCES specialty(id),
+	        ADD CONSTRAINT FK_group_to_study_mode FOREIGN KEY(study_mode_id) REFERENCES study_mode(id);
+	
+/* таблица подгруппа */
+ALTER TABLE subgroup
+	ADD CONSTRAINT FK_subgroup_to_group FOREIGN KEY(group_id) REFERENCES group(id);
+
+/* таблица подгруппа и студент */
+ALTER TABLE subgroup_vs_student
+	ADD CONSTRAINT FK_subgroup_vs_student_to_subgroup FOREIGN KEY(subgroup_id) REFERENCES subgroup(id),
+        ADD CONSTRAINT FK_subgroup_vs_student_to_student FOREIGN KEY(student_id) REFERENCES student(id);
+
+/* таблица куратор */
+ALTER TABLE curator
+	ADD CONSTRAINT FK_curator_to_group FOREIGN KEY(group_id) REFERENCES group(id),
+        ADD CONSTRAINT FK_curator_to_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id);
+
+/* таблица занятие */
+ALTER TABLE lesson
+	ADD CONSTRAINT FK_lesson_to_group FOREIGN KEY(group_id) REFERENCES group(id),
+        ADD CONSTRAINT FK_lesson_to_subgroup FOREIGN KEY(subgroup_id) REFERENCES subgroup(id),
+            ADD CONSTRAINT FK_lesson_to_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
+                ADD CONSTRAINT FK_lesson_to_type FOREIGN KEY(type_id) REFERENCES lesson_type(id),
+                    ADD CONSTRAINT FK_lesson_to_lecture_room FOREIGN KEY(room_id) REFERENCES lecture_room(id),
+                        ADD CONSTRAINT FK_lesson_to_discipline FOREIGN KEY(discipline_id) REFERENCES discipline(id),  
+                             ADD CONSTRAINT FK_lesson_to_lesson_number FOREIGN KEY(lesson_number_id) REFERENCES lesson_number(id);
+
+/* таблица студент */
+ALTER TABLE student
+    ADD CONSTRAINT FK_student_to_user FOREIGN KEY("id") REFERENCES user(id),                            
+        ADD CONSTRAINT FK_student_to_group FOREIGN KEY("group_id") REFERENCES group(id);
+
+/* таблица запрос */
+ALTER TABLE request
+	ADD CONSTRAINT FK_request_to_student FOREIGN KEY(student_id) REFERENCES student(id),
+		ADD CONSTRAINT FK_request_to_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
+			ADD CONSTRAINT FK_request_to_status FOREIGN KEY(status_id) REFERENCES status(id);
+			
+/* таблица курсовая работа */
+ALTER TABLE term_paper
+	ADD CONSTRAINT FK_term_paper_to_student FOREIGN KEY(student_id) REFERENCES student(id),
+		ADD CONSTRAINT FK_term_paper_to_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
+			ADD CONSTRAINT FK_term_paper_to_status FOREIGN KEY(status_id) REFERENCES status(id),
+		        ADD CONSTRAINT FK_term_paper_to_resource FOREIGN KEY(resource_id) REFERENCES resource(id);
+			
+/* таблица дипломная работа */
+ALTER TABLE graduation_paper
+	ADD CONSTRAINT FK_graduation_paper_to_student FOREIGN KEY(student_id) REFERENCES student(id),
+		ADD CONSTRAINT FK_graduation_paper_to_teacher FOREIGN KEY(teacher_id) REFERENCES teacher(id),
+			ADD CONSTRAINT FK_graduation_paper_to_status FOREIGN KEY(status_id) REFERENCES status(id),
+		        ADD CONSTRAINT FK_graduation_paper_to_resource FOREIGN KEY(resource_id) REFERENCES resource(id);
+			
+/* таблица практика */
+ALTER TABLE practice
+	ADD CONSTRAINT FK_practice_to_student FOREIGN KEY(student_id) REFERENCES student(id),
+		ADD CONSTRAINT FK_practice_to_organization FOREIGN KEY(organization_id) REFERENCES organization(id),		
+			ADD CONSTRAINT FK_practice_to_status FOREIGN KEY(status_id) REFERENCES status(id),
+		        ADD CONSTRAINT FK_practice_to_resource FOREIGN KEY(resource_id) REFERENCES resource(id),
+                    ADD CONSTRAINT FK_practice_to_practice_type FOREIGN KEY(type_id) REFERENCES practice_type(id);
---
+++ sql/structure.sql
@@ -0,0 +1,347 @@
+/**
+ * 1.  Нужно ли в таблице user для поля email параметр NOT NULL и UNIQUE.
+ * 2.  Просмотреть структуру таблицы source.
+
+ * 18. В старой БД не было информации об успеваемости, так что необходимо решить, в каком виде добавить эти данные:
+ *     в виде таблицы exam_result и credit, или же создать отдельную таблицу accreditation, указывающую на конкретную
+ *     сессию, а от нее сделать зависимые таблицы и для экзаменов, и для зачетов, а то, как в них будет храниться
+ *     информация, уже обговорить позже. Пока реализован первый вариант с таблицами exam_result и credit.
+
+ * 21. Нужно уточнить, как будет храниться информация о проведении и результатах защиты курсовых работ, дипломных работ и 
+ *     о результатах практики (добавить поле оценка).
+ */
+
+/* Таблица "дисциплина" */
+DROP TABLE IF EXISTS "discipline";
+CREATE TABLE "discipline" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(150)NOT NULL,
+    "short_name"            VARCHAR(20) NOT NULL
+);
+
+/* Таблица "номер занятия" */
+DROP TABLE IF EXISTS "lesson_number";
+CREATE TABLE "lesson_number" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "number"                SMALLINT    NOT NULL UNIQUE,
+    "start_time_1"          TIME        NOT NULL,
+    "end_time_1"            TIME        NOT NULL,
+    "start_time_2"          TIME        NOT NULL,
+    "end_time_2"            TIME        NOT NULL
+);
+
+/* Таблица "тип помещения" */
+DROP TABLE IF EXISTS "room_type";
+CREATE TABLE "room_type" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(50) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS "building";
+CREATE TABLE "building" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(200) NOT NULL UNIQUE
+);
+
+/* Таблица "аудитория" */
+DROP TABLE IF EXISTS "lecture_room";
+CREATE TABLE "lecture_room" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "number"                VARCHAR(5)  NOT NULL,
+    "type_id"               SMALLINT    ,
+    "building_id"           INT,
+    "seats_count"           SMALLINT         
+);
+
+/* Таблица "тип занятия" */
+DROP TABLE IF EXISTS "lesson_type";
+CREATE TABLE "lesson_type" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(30) NOT NULL UNIQUE
+);
+
+/* Таблица "должность" */
+DROP TABLE IF EXISTS "position";
+CREATE TABLE "position" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(40) NOT NULL UNIQUE
+);
+
+/* Таблица "ученая степень" */
+DROP TABLE IF EXISTS "academic_degree" ;
+CREATE TABLE "academic_degree" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(40) NOT NULL UNIQUE
+);
+
+/* Таблица "ученое звание" */
+DROP TABLE IF EXISTS "academic_rank" ;
+CREATE TABLE "academic_rank" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(40) NOT NULL UNIQUE
+);
+
+/* Таблица "роль" */
+DROP TABLE IF EXISTS "role" ;
+CREATE TABLE "role" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(25) NOT NULL UNIQUE
+);
+
+/* Таблица "пользователь" */
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "login"                 VARCHAR(50) NOT NULL UNIQUE,
+    "password"              VARCHAR(100)NOT NULL
+);
+
+DROP TABLE IF EXISTS "user_role" ;
+CREATE TABLE "user_role" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "user_id"               INT NOT NULL,
+    "role_id"               INT NOT NULL
+);
+
+/* Таблица "кафедра" */
+DROP TABLE IF EXISTS "department";
+CREATE TABLE "department" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(100)NOT NULL UNIQUE,
+    "owner_id"              INT         ,
+    "phone"                 VARCHAR(30) ,
+    "room_id"               INT         
+);
+
+/* Таблица "телефон" */
+DROP TABLE IF EXISTS "phone";
+CREATE TABLE "phone" (
+    "user_id"               INT         ,
+    "phone"                 VARCHAR(30) PRIMARY KEY
+);
+
+/* Таблица "ресурс" */
+DROP TABLE IF EXISTS "resource";
+CREATE TABLE "resource" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "description"           TEXT        NOT NULL
+);
+
+/* Таблица "информация о пользователе" */
+DROP TABLE IF EXISTS "user_info";
+CREATE TABLE "user_info" (
+    "user_id"               INT         PRIMARY KEY,
+    "description"           TEXT,
+    "birthday"              DATE        NOT NULL,
+    "city"                  VARCHAR(30) NOT NULL,
+    "address"               TEXT        NOT NULL,
+    "photo_id"              INT         
+);
+
+/* Таблица "преподаватель" */
+DROP TABLE IF EXISTS "teacher";
+CREATE TABLE "teacher" (
+    "id"                    INT         PRIMARY KEY,
+    "full_name"             VARCHAR(150)NOT NULL,
+    "short_name"            VARCHAR(50) NOT NULL,
+    "department_id"         SMALLINT    ,
+    "position_id"           SMALLINT    ,
+    "academic_degree_id"    SMALLINT    ,
+    "academic_rank_id"      SMALLINT    
+);
+
+/* Таблица "факультет" */
+DROP TABLE IF EXISTS "faculty";
+CREATE TABLE "faculty" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(100)NOT NULL UNIQUE,
+    "short_name"            VARCHAR(50) NOT NULL UNIQUE
+);
+
+/* Таблица "информация о факультете" */
+DROP TABLE IF EXISTS "info_faculty" ;
+CREATE TABLE "info_faculty" (
+    "id"                    SMALLINT    PRIMARY KEY,
+    "description"           TEXT        ,
+    "phone_number"          VARCHAR(20) NOT NULL UNIQUE
+);
+
+/* Таблица "специальность" */
+DROP TABLE IF EXISTS "specialty" ;
+CREATE TABLE "specialty" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "code"                  VARCHAR(20) NOT NULL UNIQUE,
+    "name"                  VARCHAR(100) NOT NULL UNIQUE,
+    "short_name"            VARCHAR(60) NOT NULL UNIQUE
+);
+
+/* Таблица "форма обучения" */
+DROP TABLE IF EXISTS "study_mode" ;
+CREATE TABLE "study_mode" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(45) NOT NULL UNIQUE
+);
+
+/* Таблица "группа" */
+DROP TABLE IF EXISTS "group";
+CREATE TABLE "group" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "number"                VARCHAR(4)  NOT NULL,
+    "faculty_id"            SMALLINT    ,
+    "specialty_id"          SMALLINT    ,
+    "study_mode_id"         SMALLINT    
+);
+
+/* Таблица "подгруппа" */
+DROP TABLE IF EXISTS "subgroup";
+CREATE TABLE "subgroup" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(20) NOT NULL,
+    "group_id"              INT         
+);
+
+/* Таблица "подгруппа и студент" */
+DROP TABLE IF EXISTS "subgroup_vs_student";
+CREATE TABLE "subgroup_vs_student" (
+    "id"                    SERIAL PRIMARY KEY,
+    "student_id"            INT         ,
+    "subgroup_id"           INT         
+);
+
+/* Таблица "куратор" */
+DROP TABLE IF EXISTS "curator";
+CREATE TABLE "curator" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "group_id"              INT         ,
+    "teacher_id"            INT         
+);
+
+/* Таблица "занятие" */
+DROP TABLE IF EXISTS "lesson" ;
+CREATE TABLE "lesson" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "group_id"              INT         ,
+    "subgroup_id"           INT         ,
+    "teacher_id"            INT         ,
+    "type_id"               SMALLINT    ,
+    "room_id"               SMALLINT    ,
+    "discipline_id"         SMALLINT    ,
+    "lesson_number_id"      SMALLINT    ,
+    "week_day"              SMALLINT    NOT NULL
+);
+
+/* Таблица "студент" */
+DROP TABLE IF EXISTS "student";
+CREATE TABLE "student" ( 
+    "id"                    INT         PRIMARY KEY,
+    "full_name"             VARCHAR(300) NOT NULL,
+    "short_name"            VARCHAR(100) NOT NULL,
+    "group_id"              INT         ,
+    "subgroup_id"              INT         ,
+    "record_book"           VARCHAR(30)  NOT NULL,
+);
+
+-- /* Таблица "результат экзамена" */
+-- DROP TABLE IF EXISTS "exam_result";
+-- CREATE TABLE "exam_result" (
+--     "student_id"            INT         REFERENCES "student",
+--     "discipline_id"         SMALLINT    REFERENCES "discipline",
+--     "semester"              SMALLINT    NOT NULL CHECK ("semester" > 0 AND "semester" < 11),
+--     "exam_score"            SMALLINT    NOT NULL CHECK ("exam_score" > 0 AND "exam_score" < 11),
+--     UNIQUE("student_id", "discipline_id", "semester")
+-- );
+
+-- /* Таблица "зачет" */
+-- DROP TABLE IF EXISTS "credit";
+-- CREATE TABLE "credit" (
+--     "student_id"            SMALLINT    REFERENCES "student",
+--     "discipline_id"         SMALLINT    REFERENCES "discipline",
+--     "semester"              SMALLINT    NOT NULL CHECK ("semester" > 0 AND "semester" < 11),
+--     "credit"                BOOL        NOT NULL,
+--     UNIQUE("student_id", "discipline_id", "semester")
+-- );
+
+/******************************************************************************\
+|****** Далее идет часть БД связанная с курсовыми и дипломными работами, ******|
+|****** а также практикой студентов. Т.к. я не разбираюсь, какая может   ******|
+|****** понадобиться в этих таблицах информация, то просто возьму за ос- ******|
+|****** нову те таблицы, что у нас уже есть и совмещу это таблицами, что ******|
+|****** найдены в интернете в рамках других курсовых работ.              ******|
+\******************************************************************************/
+
+/* Таблица "статус" */
+DROP TABLE IF EXISTS "status";
+CREATE TABLE "status" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(12) NOT NULL UNIQUE
+);
+
+/* Таблица "заявка" */
+DROP TABLE IF EXISTS "request";
+CREATE TABLE "request" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "student_id"            INT         ,
+    "teacher_id"            INT         ,
+    "status_id"             SMALLINT    ,
+    "topic"                 VARCHAR(50) NOT NULL,
+    "name"                  VARCHAR(90) NOT NULL,
+    "create_date"           DATE        NOT NULL DEFAULT CURRENT_DATE,
+    "update_date"           DATE        ,
+    "description"           TEXT        
+);
+
+/* Таблица "курсовая работа" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+DROP TABLE IF EXISTS "term_paper";
+CREATE TABLE "term_paper" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "student_id"            INT         ,
+    "teacher_id"            INT         ,
+    "status_id"             SMALLINT    ,
+    "topic"                 VARCHAR(50) NOT NULL,
+    "name"                  VARCHAR(90) NOT NULL,
+    "description"           TEXT        ,
+    "resource_id"           INT         
+);
+
+/* Таблица "дипломная работа" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+DROP TABLE IF EXISTS "graduation_paper";
+CREATE TABLE "graduation_paper" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "student_id"            INT         ,
+    "teacher_id"            INT         ,
+    "status_id"             SMALLINT    ,
+    "topic"                 VARCHAR(50) NOT NULL,
+    "name"                  VARCHAR(90) NOT NULL,
+    "description"           TEXT        ,
+    "source_id"             INT         
+);
+
+/* Таблица "организация" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+DROP TABLE IF EXISTS "organization";
+CREATE TABLE "organization" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(90) NOT NULL UNIQUE
+);
+
+/* Таблица "виды практик" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+DROP TABLE IF EXISTS "practice_type";
+CREATE TABLE "practice_type" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "name"                  VARCHAR(50) NOT NULL UNIQUE
+);
+
+/* Таблица "практика" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+DROP TABLE IF EXISTS "practice";
+CREATE TABLE "practice" (
+    "id"                    SERIAL      PRIMARY KEY,
+    "student_id"            INT         ,
+    "organization_id"       SMALLINT    ,
+    "status_id"             INT         ,
+    "topic"                 VARCHAR(50) NOT NULL,
+    "name"                  VARCHAR(90) NOT NULL,
+    "description"           TEXT        ,
+    "start_date"            DATE        NOT NULL,
+    "end_date"              DATE        NOT NULL,
+    "source_id"             INT         ,
+    "type_id"               INT         
+);
