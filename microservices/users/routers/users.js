97faa380fa0aa3153061cba0051df4f91cdeaea1
Arthur Evmenenko <arthur.evmenenko@gmail.com>
2019-11-17 13:05:57 +0300

    the system of routes and routers has been fixed

--- microservices/users/routers/users.js
+++ microservices/users/routers/users.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
 
-router.get('/users/:userId', userController.readById);
-router.post('/users/:userId', userController.update);
-router.delete('/users/:userId', userController.destroy);
-router.get('/users', userController.list);
-router.post('/users', userController.create);
+router.get('/:userId', userController.readById);
+router.post('/:userId', userController.update);
+router.delete('/:userId', userController.destroy);
+router.get('/', userController.list);
+router.post('/', userController.create);
 
 module.exports = router;
