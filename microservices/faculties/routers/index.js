c44873c551d9738fa2c4fe28d295256081383244
Alexandra <aleksasha43@gmail.com>
2019-11-30 17:16:38 +0300

    add routers

--- microservices/faculties/routers/index.js
+++ microservices/faculties/routers/index.js
@@ -1,5 +1,17 @@
-const userRouter = require('./faculty')
+import departmentRouter from './departmentRouter'
+import facultyRouter from './facultyRouter'
+import groupRouter from './groupRouter'
+import infoFacultyRouter from './infoFacultyRouter'
+import specialtyRouter from './specialtyRouter'
+import studyModeRouter from './studyModeRouter'
+import subgroupRouter from './subgroupRouter'
 
-module.exports = {
-    userRouter: userRouter,
+export default {
+    departmentRouter,
+    facultyRouter,
+    groupRouter,
+    infoFacultyRouter,
+    specialtyRouter,
+    studyModeRouter,
+    subgroupRouter,
 }
