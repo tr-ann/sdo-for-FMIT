8bb5ac3bda10eaf203f7977a00158fb7f21ccac0
Alexandra <aleksasha43@gmail.com>
2019-11-30 16:55:51 +0300

    delete migrations

--- microservices/students/migrations/sql/users.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE "user" (
-    "id"                    SERIAL      PRIMARY KEY,
-    "login"                 VARCHAR(50) NOT NULL UNIQUE,
-    "password"              VARCHAR(100)NOT NULL
-);
