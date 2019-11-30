9d349f8da512b1a0f029774c3ab16370ad71d88c
Alexandra <aleksasha43@gmail.com>
2019-11-30 18:34:49 +0300

    fix ruters

--- microservices/faculties/routers/infoFacultyRouter.js
+++ microservices/faculties/routers/infoFacultyRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import InfoFacultyController from '../controllers/buildingController'
 
-export default router = Router()
+const router = Router()
 
 router.get('/:id', InfoFacultyController.readById)
 router.post('/:id', InfoFacultyController.update)
 router.delete('/:id', InfoFacultyController.destroy)
 router.get('/', InfoFacultyController.readAll)
 router.post('/', InfoFacultyController.create)
+
+export default router
