9f8310bbe46a27f65a538b5a21d621b7c91e6244
Arthur Evmenenko <arthur.evmenenko@gmail.com>
2019-12-09 05:52:07 +0300

    added express-joi and validation

--- src/modules/lessons/routers/lectureRoomRouter.js
+++ src/modules/lessons/routers/lectureRoomRouter.js
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import LectureRoomController from '../controllers/lectureRoomController'
+import IdValidator from '../../../validation/id-validation'
+import LectureRoomValidator from '../validators/lecture-room-validator'
+import expressJoi from 'express-joi'
 
 const router = Router()
 
-router.get('/:id', LectureRoomController.readById)
-router.post('/:id', LectureRoomController.update)
-router.delete('/:id', LectureRoomController.destroy)
+router.get('/:id', expressJoi.joiValidate(IdValidator), LectureRoomController.readById)
+router.post('/:id', expressJoi.joiValidate(IdValidator, LectureRoomValidator), LectureRoomController.update)
+router.delete('/:id', expressJoi.joiValidate(IdValidator), LectureRoomController.destroy)
 router.get('/', LectureRoomController.readAll)
-router.post('/', LectureRoomController.create)
+router.post('/', expressJoi.joiValidate(LectureRoomValidator), LectureRoomController.create)
 
 export default router
